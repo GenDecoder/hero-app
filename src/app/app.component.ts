a161e0c3dd2bce246db2f9178848cead608976e9
GenDecoder <mariomedranomaldonado@gmail.com>
2017-02-05 20:25:44 -0400

    Changed to scss preprocesor
    Scss procesor for styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ho-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ho works!';
+  title: string = "Welcome to Heroes App with extra powers.";
+
+  onChangeTitle(): void {
+    this.title = this.title + " more";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
+/**
+ * Services
+ */
+import { HeroService } from './hero/hero.service';
+
+/**
+ * Components
+ */
 import { AppComponent } from './app.component';
+import { HeroListComponent } from './hero/hero-list/hero-list.component';
+import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+/**
+ * Routing
+ */
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroListComponent,
+    DashboardComponent    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
