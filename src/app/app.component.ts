8a160cecf5b4842e89e959aac121181d9140e9b9
GenDecoder <mariomedranomaldonado@gmail.com>
2017-02-08 01:03:14 -0400

    Router step comple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ho-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './app.component.scss'
+  ]
+
 })
 export class AppComponent {
   title: string = "Welcome to Heroes App with extra powers.";
-
-  onChangeTitle(): void {
-    this.title = this.title + " more";
-  }
 }
